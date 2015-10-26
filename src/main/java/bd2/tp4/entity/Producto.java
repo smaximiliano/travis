6920e9412827a6c15367dd406d273e92d6e20e2d
smaximiliano <maxsos92@gmail.com>
2015-10-25 22:33:02 -0300

    first commit

---
+++ src/main/java/bd2/tp4/entity/Producto.java
@@ -0,0 +1,9 @@
+package bd2.tp4.entity;
+
+public class Producto{
+	
+public Producto(){
+	System.out.println("Hello from Producto constructor");
+}
+
+}
---
+++ src/main/java/bd2/tp4/utility/Main.java
@@ -0,0 +1,11 @@
+package bd2.tp4.utility;
+
+import bd2.tp4.entity.Producto;
+
+public class Main{
+public static void main(String[] args){
+	Producto p = new Producto();
+	System.out.println("Hello ant!");
+}
+
+}
